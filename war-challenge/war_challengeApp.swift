c2bf07fa9efe7001b53464b0fc1911f049ff285e
Joon Park <aisgandy@hotmail.com>
2021-06-10 21:04:18 +0900

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by 박준우 on 2021/06/10.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
